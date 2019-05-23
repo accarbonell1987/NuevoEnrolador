62bbc5eba0ef95810fc00085c2ebd00f20020aa4
lpguio <lpguio@holguin.copextel.com.cu>
2019-05-22 17:09:41 -0700

    Pequenos cambios.

--- EnroladorAccesoDatos/SQLite/SQLiteEnrollModel.Designer.cs
+++ EnroladorAccesoDatos/SQLite/SQLiteEnrollModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Trabajo\Chile\Trabajo 2\GitHub\NuevoEnrolador\EnroladorAccesoDatos\SQLite\SQLiteEnrollModel.edmx'. 
+﻿// T4 code generation is enabled for model 'd:\Usuarios\Leandro\_NewEnroller\REPO\EnroladorAccesoDatos\SQLite\SQLiteEnrollModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
